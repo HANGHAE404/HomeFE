6169c938b47e72994ca94dbf290237e6b9dbcb3c
jjolraman <jjolraman@gmail.com>
2023-03-03 16:23:46 +0900

    add css in join.tsx file.

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import TopNav from '../page/TopNav'
 import { getUser } from '../util/localstorage'
 import Home from '../page/Home'
+import Join from '../page/Join'
+
 const Router = () => {
   const userInfo = getUser()
   return (
@@ -9,6 +11,7 @@ const Router = () => {
       <Routes>
         <Route element={<TopNav />}>
           <Route path="/" element={<Home />} />
+          <Route path="/join" element={<Join />} />
         </Route>
       </Routes>
     </BrowserRouter>
